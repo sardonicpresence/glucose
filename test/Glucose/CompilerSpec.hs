009722f409cca4aff1e366abdebbb3daf769fdde
Neil Vice <sardonicpresence@gmail.com>
2017-11-16 09:40:52 +0800

    Add functions, calls & JavaScript name-mangling to end-to-end test

--- test/Glucose/CompilerSpec.hs
+++ test/Glucose/CompilerSpec.hs
@@ -6,7 +6,7 @@ import Control.Comonad
 import Data.Foldable
 import Data.Maybe (fromJust)
 import Data.Monoid ((<>))
-import Data.Text
+import Data.Text (Text, unpack)
 import Glucose.Codegen
 import Glucose.Codegen.Target
 import Glucose.Compiler
@@ -16,7 +16,9 @@ source :: Text
 source = "b=\x5d0\&_a0\n" <>
          "\x5d0\&_a0 =\xa0\&123e01\n" <>
          "_b\x5d5\&0=12.3e-01\f" <>
-         "type\xa0\&\x5d1_=  \x5d2_|\x5d3"
+         "type\xa0\&\x5d1_=  \x5d2_|\x5d3\n" <>
+         "id=\\a->a\f" <>
+         "const\xa0\&=\\\x5d4\&->id\xa0\x5d4"
 
 spec :: Spec
 spec = describe "compile" $ do
@@ -25,20 +27,29 @@ spec = describe "compile" $ do
   it "fails on unexpected operator" . forAllOutputs $ \t ->
     compile t " \n$~" `shouldErrorContaining` "$~"
   it "correctly compiles utf8 example to LLVM" $
-    unpack <$> compile (codegenDefinitions $ LLVM target) source `shouldBe`
-      Right ("@b = unnamed_addr alias i32, i32* @$5d0$_a0\n" ++
-             "@$5d0$_a0 = unnamed_addr constant i32 1230\n" ++
-             "@_b$5d5$0 = unnamed_addr constant double 1.23\n" ++
-             "@$5d2$_ = unnamed_addr constant i32 0\n" ++
-             "@$5d3$ = unnamed_addr constant i32 1\n")
+    unpack <$> compile (codegenDefinitions $ LLVM target) source `shouldBe` (Right . unlines)
+      [ "@b = unnamed_addr alias i32, i32* @$5d0$_a0"
+      , "@$5d0$_a0 = unnamed_addr constant i32 1230"
+      , "@_b$5d5$0 = unnamed_addr constant double 1.23"
+      , "@$5d2$_ = unnamed_addr constant i32 0"
+      , "@$5d3$ = unnamed_addr constant i32 1"
+      , "define fastcc %$box @id(%$box %a) unnamed_addr #0 {"
+      , "  ret %$box %a"
+      , "}"
+      , "define fastcc %$box @const(%$box %$5d4$) unnamed_addr #0 {"
+      , "  %1 = tail call fastcc %$box @id(%$box %$5d4$)"
+      , "  ret %$box %1"
+      , "}" ]
   it "correctly compiles utf8 example to JavaScript" $
-    unpack <$> compile (codegenDefinitions JavaScript) source `shouldBe`
-      Right ("\x5d0\&_a0 = 1230\n" ++
-             "b = \x5d0\&_a0\n" ++
-             "_b\x5d5\&0 = 1.23\n" ++
-             "\x5d1_ = function() {}\n" ++
-             "\x5d2_ = new \x5d1_()\n" ++
-             "\x5d3 = new \x5d1_()\n")
+    unpack <$> compile (codegenDefinitions JavaScript) source `shouldBe` (Right . unlines)
+      [ "\x5d0\&_a0 = 1230"
+      , "b = \x5d0\&_a0"
+      , "_b\x5d5\&0 = 1.23"
+      , "\x5d1_ = function() {}"
+      , "\x5d2_ = new \x5d1_()"
+      , "\x5d3 = new \x5d1_()"
+      , "function id(a) { return a }"
+      , "function $const(\x5d4) { return id(\x5d4) }" ]
 
 forAllOutputs :: (Comonad f, FormattableFunctor Format f, Applicative m) => (Codegen f -> m a) -> m ()
 forAllOutputs f = traverse_ (f . codegen) [LLVM target, JavaScript]
